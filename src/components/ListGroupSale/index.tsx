5177aaa510bf82dfb9d18919cadff0dad2ab5504
Hanh <hanh.hm@hesperiafreepress.com>
2021-12-30 03:15:38 -0800

    style components detail group sale ,progres,card

---
+++ src/components/ListGroupSale/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from './index.less';
+import classNames from 'classnames';
+import 'rc-checkbox/assets/index.css';
+import Text from '../Text';
+import Progress from '@/components/Progress';
+import Button from '@/components/Button';
+
+interface ListGroupSaleProps {
+  className?: string;
+  label?: string;
+  disabled?: boolean;
+}
+const ListGroupSale = React.forwardRef(
+  (props: ListGroupSaleProps, ref: any) => {
+    const { className, label, disabled = false, ...rest } = props;
+    const Dropdown = require('rc-dropdown');
+
+    const classes: string = classNames(styles.default, className);
+
+    return (
+      <div className={styles.ListGroupSaleWrapper}>
+        <div className={styles.background}>
+          <div className={styles.backgroundContent}>
+            <div className={styles.left}>
+              <div></div>
+              <div></div>
+            </div>
+            <div className={styles.dropdown}>
+              <Dropdown />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  },
+);
+
+export default React.memo(ListGroupSale);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
 import 'rc-dialog/assets/index.css';
 import styles from './index.less';
 import '@/global/theme.less';
+import Step from '@/components/Step';
+import Card from '@/components/Card';
+import Progress from '@/components/Progress';
+import DetailGroupSale from '@/components/DetailGroupSale';
+import ListGroupSale from '@/components/ListGroupSale';
 
 export default function IndexPage() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div style={{ marginTop: 20, paddingBottom: 200, marginLeft: 20 }}>
+      <Step />
+      <Card />
+      <Progress />
+      <DetailGroupSale />
+      <ListGroupSale />
     </div>
   );
 }
