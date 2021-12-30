11cbc11936cb36ebaba505dfd08f125bba3f908e
Hanh <hanh.hm@hesperiafreepress.com>
2021-12-29 19:15:54 -0800

    style components header footer

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
-
+import Text from '../../components/Text';
 import styles from './index.less';
 
 function Footer() {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <div className={styles.footerWrapper}>
+      <div className={styles.footerFlex}>
+        <div className={styles.copyright}>
+          <Text type="body-p1-regular" color="neutral-100">
+            Copyright © 2021 Staking. All rights reserved.
+          </Text>
+        </div>
+        <div className={styles.footerContact}>
+          <div className={styles.footerContactIcon}>
+            <span className={styles.icon}>
+              <img src="/assets/images/Vector.svg" alt="telegram" />
+            </span>
+            <span className={styles.text}>
+              <a href="#">
+                <Text type="body-p1-regular" color="neutral-100">
+                  Join us on Telegram
+                </Text>
+              </a>
+            </span>
+          </div>
+          <div className={styles.footerContactIcon}>
+            <span className={styles.icon}>
+              <img src="/assets/images/_x30_4.Twitter.svg" alt="twitter" />
+            </span>
+            <span className={styles.text}>
+              <a href="#">
+                <Text type="body-p1-regular" color="neutral-100">
+                  Follow our Twitter
+                </Text>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
