12837f44e72932802dd3845090d9b1493b610b22
Hanh <hanh.hm@hesperiafreepress.com>
2022-01-04 03:09:01 -0800

    style launchpad

--- src/pages/Launchpad/ListGroupSale/index.tsx
+++ src/pages/Launchpad/ListGroupSale/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styles from './index.less';
 import classNames from 'classnames';
 import 'rc-checkbox/assets/index.css';
-import Text from '../Text';
+import Text from '@/components/Text';
 import Progress from '@/components/Progress';
 import Button from '@/components/Button';
 import Dropdown from 'rc-dropdown';
@@ -46,39 +46,37 @@ const ListGroupSale = React.forwardRef(
       >
         <div style={{ padding: '0px 32px' }}>
           <div className={styles.background}>
-            <div className={styles.backgroundContent}>
-              <div className={styles.flexBetween}>
-                <div className={styles.flex}>
-                  <div className={styles.logo}>
-                    <img src="/assets/images/logoSale.svg" />
+            <div className={styles.flexBetween}>
+              <div className={styles.flex}>
+                <div className={styles.logo}>
+                  <img src="/assets/images/logoSale.svg" />
+                </div>
+                <div className={styles.LunaRush}>
+                  <Text type="heading-p3-bold" color="neutral-100">
+                    Luna Rush
+                  </Text>
+                  <div className={styles.icon}>
+                    <a href="#">
+                      <img src="/assets/images/Group.svg" />
+                    </a>
+                    <a href="#">
+                      <img src="/assets/images/Twitter.svg" />
+                    </a>
+                    <a href="#">
+                      <img src="/assets/images/ic-telegram.svg" />
+                    </a>
                   </div>
-                  <div className={styles.LunaRush}>
-                    <Text type="heading-p3-bold" color="neutral-100">
-                      Luna Rush
-                    </Text>
-                    <div className={styles.icon}>
-                      <a href="#">
-                        <img src="/assets/images/Group.svg" />
-                      </a>
-                      <a href="#">
-                        <img src="/assets/images/Twitter.svg" />
-                      </a>
-                      <a href="#">
-                        <img src="/assets/images/ic-telegram.svg" />
-                      </a>
-                    </div>
-                  </div>
-                  <div className={styles.buttonLusBush}>
-                    <button className={styles.buttonLus}>LUS / BUSD</button>
-                  </div>
+                </div>
+                <div className={styles.buttonLusBush}>
+                  <button className={styles.buttonLus}>LUS / BUSD</button>
+                </div>
 
-                  <button className={styles.buttonLus}> LUS</button>
-                </div>
-                <div className={styles.dropdown}>
-                  <button onClick={() => setDropdown(!dropdown)}>
-                    <img src="/assets/images/dropdown.svg" />
-                  </button>
-                </div>
+                <button className={styles.buttonLus}> LUS</button>
+              </div>
+              <div className={styles.dropdown}>
+                <button onClick={() => setDropdown(!dropdown)}>
+                  <img src="/assets/images/dropdown.svg" />
+                </button>
               </div>
             </div>
           </div>
--- src/pages/Launchpad/index.tsx
+++ src/pages/Launchpad/index.tsx
@@ -1,9 +1,154 @@
 import styles from './index.less';
+import Text from '@/components/Text';
+import ListGroupSale from '../Launchpad/ListGroupSale';
+import React, { useState } from 'react';
 
 export default function LaunchpadPage() {
+  const [activeTab, setActiveTab] = useState('next-ido');
   return (
-    <div>
-      <h1>Launchpad</h1>
+    <div className={styles.wrapperLaunch}>
+      <div className={styles.heading}>
+        <div className={styles.left}>
+          <div className={styles.title}>Agile Launchpad</div>
+          <Text color="neutral-200" type="body-p1-regular">
+            Agile LaunchPad projects are carefully chosen through vetting, which
+            takes everything from the origins of the project teams to legitimacy
+            and implementation capacities into account
+          </Text>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.item}>
+            <span>
+              <Text color="neutral-100" type="subheading-p1-bold">
+                Wallet Address:
+              </Text>
+            </span>
+            <span className={styles.itemContent}>
+              <img src="/assets/images/ic-copy.svg" alt="copy" />
+            </span>
+          </div>
+          <div className={styles.flex}>
+            <div className={styles.leftContent}>
+              <div className={styles.item}>
+                <span className={styles.itemTitle}>
+                  <Text color="neutral-100" type="subheading-p1-bold">
+                    Balance:
+                  </Text>
+                </span>
+                <span className={styles.itemContent}>
+                  <Text color="primary-violet" type="subheading-p1-bold">
+                    0
+                  </Text>
+                </span>
+              </div>
+              <div className={styles.item}>
+                <span className={styles.itemTitle}>
+                  <Text color="neutral-100" type="subheading-p1-bold">
+                    Staking:
+                  </Text>
+                </span>
+                <span className={styles.itemContent}>
+                  <Text color="primary-violet" type="subheading-p1-bold">
+                    0
+                  </Text>
+                </span>
+              </div>
+              <div className={styles.item}>
+                <span className={styles.itemTitle}>
+                  <Text color="neutral-100" type="subheading-p1-bold">
+                    Current tier:
+                  </Text>
+                </span>
+                <span className={styles.itemContent}>
+                  <Text color="primary-violet" type="subheading-p1-bold">
+                    N/A
+                  </Text>
+                </span>
+              </div>
+            </div>
+            <div className={styles.rightContent}>
+              <div>
+                <div className={styles.item}>
+                  <span className={styles.itemTitle}>
+                    <Text color="neutral-100" type="subheading-p1-bold">
+                      NFT Boost:
+                    </Text>
+                  </span>
+                  <span className={styles.itemContent}>
+                    <Text color="primary-violet" type="subheading-p1-bold">
+                      No
+                    </Text>
+                  </span>
+                </div>
+                <div className={styles.item}>
+                  <span className={styles.itemTitle}>
+                    <Text color="neutral-100" type="subheading-p1-bold">
+                      Staking Boost:
+                    </Text>
+                  </span>
+                  <span className={styles.itemContent}>
+                    <Text color="primary-violet" type="subheading-p1-bold">
+                      0
+                    </Text>
+                  </span>
+                </div>
+                <div className={styles.item}>
+                  <span className={styles.itemTitle}>
+                    <Text color="neutral-100" type="subheading-p1-bold">
+                      Additional Pool Weight:
+                    </Text>
+                  </span>
+                  <span className={styles.itemContent}>
+                    <Text color="primary-violet" type="subheading-p1-bold">
+                      N/A
+                    </Text>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.tabLaunch}>
+        <button
+          className={
+            activeTab === 'next-ido' ? styles.tabButtonActive : styles.tabButton
+          }
+          onClick={() => setActiveTab('next-ido')}
+        >
+          <Text type="body-p1-bold" color="primary-violet">
+            Next IDO
+          </Text>
+        </button>
+        <button
+          className={
+            activeTab === 'open-ido' ? styles.tabButtonActive : styles.tabButton
+          }
+          onClick={() => setActiveTab('open-ido')}
+        >
+          <Text type="body-p1-bold" color="primary-violet">
+            Open IDOs
+          </Text>
+        </button>
+        <button
+          className={
+            activeTab === 'past-ido' ? styles.tabButtonActive : styles.tabButton
+          }
+          onClick={() => setActiveTab('past-ido')}
+        >
+          <Text type="body-p1-bold" color="primary-violet">
+            Past IDOs
+          </Text>
+        </button>
+      </div>
+      <div className={styles.ListGroupSale}>
+        <div className={styles.ListGroupSaleItem}>
+          <ListGroupSale />
+        </div>
+        <div className={styles.ListGroupSaleItem}>
+          <ListGroupSale />
+        </div>
+      </div>
     </div>
   );
 }
